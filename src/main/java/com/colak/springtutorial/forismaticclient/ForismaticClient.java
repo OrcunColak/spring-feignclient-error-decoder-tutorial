35e25d408f95b07ee86ebb00565df7da5612ef63
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-24 10:34:14 +0300

    use explicit config

--- src/main/java/com/colak/springtutorial/forismaticclient/ForismaticClient.java
+++ src/main/java/com/colak/springtutorial/forismaticclient/ForismaticClient.java
@@ -1,12 +1,13 @@
 package com.colak.springtutorial.forismaticclient;
 
+import com.colak.springtutorial.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 // Define the Feign client for Forismatic API
-@FeignClient(name = "forismaticClient", url = "${forismatic.api.url}")
+@FeignClient(name = "forismaticClient", url = "${forismatic.api.url}", configuration = FeignConfig.class)
 public interface ForismaticClient {
 
     // Mapping for the getQuote method in Forismatic API
